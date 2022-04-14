550001da51c93154472694812334e374475f183f
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2022-04-15 00:39:06 +0200

    navbar

--- index.php
+++ index.php
@@ -8,8 +8,26 @@
     <title>Home</title>
 </head>
     <body style="background-color:#343a40">
-    <div class="container" style="padding:20px;border:double">
-        <h1 style="color:white;">Titolo</h1>
+    <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <img src="./img/exit.png" style="background:white;width:40px;">
+            <h3 style="padding-top: 8px; padding-left:5px">Homescape</h3>
+        </a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" style="padding-top:10px; padding-left:5px">
+          <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Chi siamo</a></li>
+          <li><a href="#" class="nav-link px-2 text-warning">Classifica</a></li>
+        </ul>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Accedi</button>
+          <button type="button" class="btn btn-warning">Registrati</button>
+        </div>
+      </div>
     </div>
+    
+  </header>
 </body>
 </html> 
