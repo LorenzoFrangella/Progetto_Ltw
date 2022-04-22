7337ff0fdf74e8f9c3590fe4942fea42c772907a
Pigiby <pgb_2000@outlook.it>
2022-04-22 17:01:31 +0200

    siuuuu

--- ceck_registrazione.php
+++ ceck_registrazione.php
@@ -5,7 +5,7 @@ if (!(isset($_POST['reg']))) {
 }
 else {
     $dbconn = pg_connect("host=localhost port=5432 dbname=HousEscape 
-                user=postgres password=lorenzo") 
+                user=postgres password=biar") 
                 or die('Could not connect: ' . pg_last_error());
 }
 ?>
