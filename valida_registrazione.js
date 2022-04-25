d8b55fa1d32e670c3452b5b3b4caeeda66d57e78
Pigiby <pgb_2000@outlook.it>
2022-04-25 18:21:58 +0200

    SIUIOI

--- valida_registrazione.js
+++ valida_registrazione.js
@@ -1,32 +1,37 @@
 function valida_registrazione(){
     var debug = true;
         if (document.HousEscape.nome.value=="") {
-        if(debug)alert("Inserire nome");
+        //if(debug)alert("Inserire nome");
         document.HousEscape.nome.focus();
         document.getElementById("errore").innerHTML="Inserire nome";
         return false;
      }
     if (document.HousEscape.cognome.value=="") {
-        if(debug)alert("Inserire cognome");
+        //if(debug)alert("Inserire cognome");
         cognome = document.getElementById("cognome");
         cognome.focus();
         document.getElementById("errore").innerHTML = "Inserire cognome";
         return false;
         }
     if (document.HousEscape.email.value=="") {
-        if(debug)alert("Inserire l'email");
+        //if(debug)alert("Inserire l'email");
         document.HousEscape.email.focus();
         document.getElementById("errore").innerHTML = "Inserire l'email";
         return false;
         }
     if (document.HousEscape.nickname.value=="") {
-        if(debug)alert("Inserire nickname");
+        //if(debug)alert("Inserire nickname");
         document.HousEscape.nickname.focus();
         document.getElementById("errore").innerHTML = "Inserire nickname";
         return false;
         }
+    var n = document.HousEscape.nickname.value;
+    if(n.length < 8 || n.length > 20){
+            if(debug)alert("il nickname deve contenere da 8 a 20 caratteri");
+            return false;
+        }
     if (document.HousEscape.password.value=="") {
-        if(debug)alert("Inserire password");
+        //if(debug)alert("Inserire password");
         document.HousEscape.password.focus();
         document.getElementById("errore").innerHTML = "Inserire password";
         return false;
