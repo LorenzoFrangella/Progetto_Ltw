dcf865254b030e2bbc2c8173b87279ccf9119155
Pigiby <pgb_2000@outlook.it>
2022-04-28 11:59:20 +0200

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -26,13 +26,11 @@ if(!isset($_POST['log'])){
                 $data = pg_query_params($dbconn,$q2,array($nick_email,$password));
                 if($data){
                     echo "<h1> Login completed successfully</h1>";
-                    //$_SESSION['email'] = $nick_email;
                     $q3 = "select nickname from utente where email = $1 or nickname = $1";
                     $result = pg_query_params($dbconn,$q3,array($nick_email));
 
                     $_SESSION['nickname'] = pg_fetch_result($result,0,0);
-                    //echo $_SESSION['nickname'];
-                   // echo $_SESSION['nickname'];
+                    echo $_SESSION['nickname'];
 
                     
                 }
