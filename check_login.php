04e4571374b8ce196f9519cafba775cc5e39659c
Pigiby <pgb_2000@outlook.it>
2022-04-28 11:22:04 +0200

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -1,8 +1,8 @@
 <?php
-/*if(!isset($_POST['log'])){
+if(!isset($_POST['log'])){
     header("Location: /");
 }
-*/{
+{
     $dbconn = pg_connect("host=localhost port=5432 dbname=HousEscape 
     user=postgres password=biar")
     or die ('Could not connect: '. pg_last_error());//questo va levato!!! 
