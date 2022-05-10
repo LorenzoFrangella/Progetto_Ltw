8cd5fd361175faa2b7dd87b38d32a8b4f6ca065a
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2022-05-10 13:03:22 +0200

    .

--- primo_livello.php
+++ primo_livello.php
@@ -7,10 +7,17 @@ $_SESSION['begin_game'] = true;
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <link rel="stylesheet" href="./primolivello.css">
-        <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/exit.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./stile.css">
+    <title>HousEscape</title>
 </head>
 <body>
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <a href="./index.php" class="logo d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
@@ -20,15 +27,9 @@ $_SESSION['begin_game'] = true;
         </button>
     </div>
     </nav>
-    <div class="bg" onmousemove="moveTorch(event)"></div>
-    <div class="torch" ></div>
-    <div class= "cursor"> </div>
-    <script>
-        function moveTorch(event){
-            var torch = document.getElementsByClassName("torch")[0];
-            torch.style.clipPath = `circle(100px at ${event.offsetX}px ${event.offsetY}px)`;
-        }
-        </script>
+    <div class="container format" >
+        <iframe class="inpage" src="page1.php" frameborder="0"></iframe>
+    </div>
     
 </body>
 </html>
