d4113dc056c6769bb4a85efc871f0fcacb70b4f0
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2022-05-10 11:37:14 +0200

    Update primo_livello.php

--- primo_livello.php
+++ primo_livello.php
@@ -4,14 +4,16 @@ $_SESSION['begin_game'] = true;
 //if(!isset($_SESSION['nickname']))header("Location: ./login.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="shortcut icon" href="./img/exit.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./stile.css">
+    <title>HousEscape</title>
 </head>
 <body>
-    Questo è il primo livello
 </body>
 </html>
