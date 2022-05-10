a2915ac3a9a44bb6a85f7f9c7156ab801a2aebea
Pigiby <pgb_2000@outlook.it>
2022-05-10 18:43:49 +0200

    koala

--- page1.php
+++ page1.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="primolivello.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="./js/jquery.rwdImageMaps.js"></script>
+    
     <title>Document</title>
 </head>
 <body>
@@ -14,17 +15,25 @@
     <div class="torch" >
       <img src="./img/casa_quattro.png" usemap="#interruttore" id="luce" width="1360" height="765">
       <map name="interruttore">
-        <area shape="rect" alt="parte 1 immagine" coords="93,238,127,282" href="./login.php" title="interruttore">
+        <area shape="rect" alt="parte 1 immagine" coords="93,238,127,282" href="#" onclick="illuminastanza();" title="interruttore">
 </map>
     </div>
 </div>
     <script>
+        var scala = 0.07;
+        var acceso = "off";
+        function illuminastanza(){
+            acceso = "on";
+            scala = 2;
+            document.body.style.cursor = "default";
+            //document.getElementById("torch")[0].style.removeProperty("clipPath");
+    };
         function moveTorch(event){
             var torch = document.getElementsByClassName("torch")[0];
             //alert($(window).height());
-            var r = $(window).width()*0.07;
+            var r = $(window).width()*scala;
             torch.style.clipPath = `circle(${r}px at ${event.offsetX}px ${event.offsetY}px)`;
-        }
+    }
         </script>
         <script>
             $(document).ready(function(e) {
