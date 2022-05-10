5fd9fbb5d6d816513a6c9ebbb05e47fbeaad0b52
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2022-05-10 12:20:30 +0200

    .

--- primo_livello.php
+++ primo_livello.php
@@ -11,6 +11,15 @@ $_SESSION['begin_game'] = true;
         <title>Document</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a href="./index.php" class="logo d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="./img/exit.png" style="width:40px;">
+          <h3 style="padding-top: 8px; padding-left:5px">HousEscape</h3>
+        </a>
+        </button>
+    </div>
+    </nav>
     <div class="bg" onmousemove="moveTorch(event)"></div>
     <div class="torch" ></div>
     <script>
