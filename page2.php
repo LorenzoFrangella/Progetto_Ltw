615bc9aacee64cfa0a2e889992aab7a9c66ac77f
Francesca Frangella <lorenzofrangellalollo@gmail.com>
2022-05-13 00:31:00 +0200

    cose

--- page2.php
+++ page2.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="giochi/piano/style.css">
     <link rel="stylesheet" href="primolivello.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="./js/jquery.rwdImageMaps.js"></script>
     <script src="./housescape.js"></script>
+    <script> var solved_piano = false</script>
     <title>Document</title>
 </head>
 <body id="secondo">
@@ -19,7 +19,7 @@
         <map name="piano">
             <area id = "area" shape="rect" alt="parte 1 immagine" coords="309,526,530,562" href="#" onclick="popup_piano();" title="piano">
         </map>
-
+        <div class="test">
         <div class="pianoforte" style="position:absolute;z-index:9999;top:37%;left:40%;display: none;">
         <button class="white-key C4-key" onclick="array_utente.push('Do')">Do</button>
         <button class="black-key Db4-key"></button>
@@ -27,38 +27,57 @@
         <button class="black-key Eb4-key"></button>
         <button class="white-key E4-key" onclick="array_utente.push('Mi')">Mi</button>
         <button class="white-key F4-key" onclick="array_utente.push('Fa')">Fa</button>
-      <button class="black-key Gb4-key"></button>
-      <button class="white-key G4-key" onclick="array_utente.push('Sol')">Sol</button>
-      <button class="black-key Ab4-key"></button>
-      <button class="white-key A4-key" onclick="array_utente.push('La')">La</button>
-      <button class="black-key Bb4-key"></button>
-      <button class="white-key B4-key" onclick="array_utente.push('Si')">Si</button>
+        <button class="black-key Gb4-key"></button>
+        <button class="white-key G4-key" onclick="array_utente.push('Sol')">Sol</button>
+        <button class="black-key Ab4-key"></button>
+        <button class="white-key A4-key" onclick="array_utente.push('La')">La</button>
+        <button class="black-key Bb4-key"></button>
+        <button class="white-key B4-key" onclick="array_utente.push('Si')">Si</button>
 </br>
 </div>
+</div>
 <p id="close">Invia la tua soluzione!</p>
+<p id="exit">Esci</p>
+<p id="progress">0/5</p>
 </div>
 <script>
     $("#area").click(function () { 
+        if(!solved_piano){
         $("#area").css("cursor","default");
         $(".pianoforte").fadeIn(500);
         $("#close").fadeIn(500);
         $("#melodia").addClass("blur");
+        $("#exit").fadeIn(500);
+        $("#progress").fadeIn(500);
+        }
   });
-  $("#close").click(function () { 
+  $("#exit").click(function(){
     $(".pianoforte").fadeOut(500);
     $("#close").fadeOut(500);
     $("#melodia").removeClass("blur");
+    $("#exit").fadeOut(500);
+    $("#progress").fadeOut(500);
+  })
+
+
+
+  $("#close").click(function () { 
     console.log(array_utente);
     if (JSON.stringify(array_utente) === JSON.stringify(mio_array)) {
         $("#area").unbind('click').removeAttr("onclick")[0].onclick = null;
         $("#area").css("cursor","default");
         alert('Bravo! La sequenza è corretta');
-        return true;
+        $(".pianoforte").fadeOut(500);
+        $("#close").fadeOut(500);
+        $("#melodia").removeClass("blur");
+        $("#exit").fadeOut(500);
+        $("#progress").fadeOut(500);
+        solved_piano = true;
     }
     else{
         array_utente=[];
         alert('Ritenta');
-        return false;
+        solved_piano= false;
     }
 });
   
