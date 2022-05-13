b0276c5685785ecfc8f7230d7a4636ad14b54c3b
Pigiby <pgb_2000@outlook.it>
2022-05-13 16:04:11 +0200

    orologio

---
+++ orologio analitico/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <div id="clockContainer">
+        <div id="hour"></div>
+        <div id="minute"></div>
+    </div>
+   <input type="time" id="time">
+   <button id="orario" onclick="inserisci_orario()">inserisci orario </button> 
+</body>
+</html>
---
+++ orologio analitico/script.js
@@ -0,0 +1,11 @@
+function inserisci_orario(){
+    var t = document.getElementById("time").value;
+    var orario = t.split(":");
+    var hr = orario[0];
+    var min = orario[1];
+    var hr_rotation = 30 * hr + min / 2; //converting current time
+    var min_rotation = 6 * min;
+  
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+}
