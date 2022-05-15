cf6385c286415a8ac9340d0fd1488bd5f3af729d
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2022-05-15 21:03:43 +0200

    scaricato i file del timer

--- primo_livello.php
+++ primo_livello.php
@@ -18,6 +18,11 @@ $_SESSION['begin_game'] = true;
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="./housescape.js"></script>
+    <script src="./timer/timer.js"></script>
+    <link rel="stylesheet" href="">
+
+
+
     <link rel="stylesheet" href="./stile.css">
     <title>HousEscape</title>
 </head>
---
+++ timer/timer.js
@@ -0,0 +1,30 @@
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    function addHours(numOfHours, date = new Date()) {
+        date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
+      
+        return date;
+    }
+    
+    let result = addHours(1);
+
+    const countDown = new Date(result).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+            distance = countDown - now;
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+              //funzione da chiamare allo scadere del tempo
+          }
+          //seconds
+        }, 100)
+    }());
