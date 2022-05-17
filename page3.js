c4773ac0a04fc860cfe3f120ded21ceb272d9873
LorenzoFrangella <lorenzofrangellalollo@gmail.com>
2022-05-17 16:05:14 +0200

    .

--- page3.js
+++ page3.js
@@ -9,6 +9,10 @@ $("#type_writer").rwdImageMaps();
 $("#tre").on('input', function() {
     terzo_enigma();
 });
+function chiudi_mds(){$("#type").css("cursor","pointer");
+    $("#type_writer").removeClass("blur");
+    $("#macchina_da_scrivere").fadeOut(500);
+    }
 function terzo_enigma(){
     var mirror = document.getElementById("tre").value;
     if(mirror=="specchio"){
@@ -16,10 +20,9 @@ function terzo_enigma(){
         $("#type").css("cursor","default");
         $("#type").unbind('click').removeAttr("onclick")[0].onclick = null;
         parent.postMessage("terzo_enigma","*");
+        chiudi_mds();
     }
 };
 $("#esci_mds").click(function(){
-    $("#type").css("cursor","pointer");
-    $("#type_writer").removeClass("blur");
-    $("#macchina_da_scrivere").fadeOut(500);
+    chiudi_mds();
 });
