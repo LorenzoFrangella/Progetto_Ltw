a392674951b94ed485316189e17e74e5effa898c
Pigiby <pgb_2000@outlook.it>
2022-05-17 15:45:41 +0200

    okoko

--- page3.js
+++ page3.js
@@ -8,17 +8,16 @@ $("#type_writer").rwdImageMaps();
 });
 $("#terzo_enigma").on('input', function() {
     terzo_enigma();
-})
+});
 function terzo_enigma(){
-    var mirror = document.getElementById("#terzo_enigma").value;
-    alert(mirror);
+    var mirror = document.getElementById("terzo_enigma").value;
     if(mirror=="specchio"){
         alert("parola corretta");
         $("#type").css("cursor","default");
         $("#type").unbind('click').removeAttr("onclick")[0].onclick = null;
         parent.postMessage("terzo_enigma","*");
     }
-}
+};
 $("#esci_mds").click(function(){
     $("#type").css("cursor","pointer");
     $("#type_writer").removeClass("blur");
