84e076d999e364e231f59ea6986c1888cc74aaaa
Pigiby <pgb_2000@outlook.it>
2022-05-19 17:10:51 +0200

    pusho timer

---
+++ tempo_scaduto.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../stile.css">
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/bootstrap.esm.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <title>HouseEscape</title>
+    <link rel="icon" href="../img/exit.png">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a href="./index.php" class="logo d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="./img/exit.png" style="width:40px;">
+          <h3 style="padding-top: 8px; padding-left:5px">HousEscape</h3>
+        </a>
+      </div>
+    </nav>
+    <div class="container" style="text-align:center">
+    <h1 class="titolo animated bounceInDown" style="text-align:center; height:300px">Noooooooo!</h1> <br>
+    <br>
+    <h2 class="animated zoomIn">Peccato, hai esaurito il tempo a tua disposizione...</h2>  
+    <br>
+    <p style="color:white;">Torna alla <a href="./index.php">home page </a> per poter fare un altro tentativo.</p>
+    <br>
+    <br>
+    </div>
+</body>
+</html>
