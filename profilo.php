ad84bd10fdcc50fe36cc7287b3ae2a20c78cb113
Pigiby <pgb_2000@outlook.it>
2022-05-19 17:47:39 +0200

    ho committato porco dio

--- profilo.php
+++ profilo.php
@@ -2,6 +2,19 @@
     session_start();
     if(!isset($_SESSION['nickname']))
         header("Location: ./login.php");
+    else {
+        $dbconn = pg_connect("host=localhost port=5432 dbname=HousEscape user=postgres password=biar") 
+            or die('Could not connect: ' . pg_last_error()); //questo va levato!!!
+        if ($dbconn) {
+            $nickname = $_SESSION['nickname'];
+            $q1= "select * from utente where nickname = $1";
+            $result = pg_query_params($dbconn, $q1, array($nickname));
+            $row = pg_fetch_row($result);
+            $nome = $row[0];
+            $cognome = $row[1];
+            $email = $row[3];
+            }
+    }
     
 ?>
 <!DOCTYPE html>
@@ -16,5 +29,21 @@
     <title>HousEscape</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a href="./index.php" class="logo d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="./img/exit.png" style="width:40px;">
+          <h3 style="padding-top: 8px; padding-left:5px">HousEscape</h3>
+        </a>
+      </div>
+    </nav>
+    <div class = "logo_utente">
+        <img src = "./img/profilo.jpg">
+        <ul>
+            <li>$nome</li>
+            <li>$cognome</li>
+            <li>$email</li>
+        </ul>
+    </div>
 </body>
 </html>
